cc7052aab4a146af1b5b0fc8ca3f1fb26e97ee25
Orazio Spina <orazio.spina@studio.unibo.it>
2024-12-31 18:36:35 +0100

    Added login implementation

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const signupRadio = document.getElementById("signup");
+    const loginRadio = document.getElementById("login");
+    const signupForm = document.getElementById("signup-form");
+    const loginForm = document.getElementById("login-form");
+    const signupContainer = document.getElementById("signup-container");
+    const loginContainer = document.getElementById("login-container");
+
+    // Inizializza visibilità
+    signupForm.style.display = "none"; // Nasconde il form di signup
+
+    // Aggiungi evento al radio button
+    signupRadio.addEventListener("change", function () {
+        if (signupRadio.checked) {
+            signupForm.style.display = "block";
+            signupContainer.style.backgroundColor = "white";
+            loginForm.style.display = "none";
+            loginContainer.style.backgroundColor = "#bdbdbd";
+        }
+    });
+
+    loginRadio.addEventListener("change", function () {
+        if (loginRadio.checked) {
+            loginForm.style.display = "block";
+            loginContainer.style.backgroundColor = "white";
+            signupForm.style.display = "none";
+            signupContainer.style.backgroundColor = "#bdbdbd";
+        }
+    });
+});
