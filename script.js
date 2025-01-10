8be99176295b6c8965a8169719fdfb2f01246110
Orazio Spina <orazio.spina@studio.unibo.it>
2025-01-10 18:07:22 +0100

    Done desktop seller implementation

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const signupRadio = document.getElementById("signup");
-    const loginRadio = document.getElementById("login");
-    const signupForm = document.getElementById("signup-form");
-    const loginForm = document.getElementById("login-form");
-    const signupContainer = document.getElementById("signup-container");
-    const loginContainer = document.getElementById("login-container");
+const signupRadio = document.getElementById("signup");
+const loginRadio = document.getElementById("login");
+const signupForm = document.getElementById("signup-form");
+const loginForm = document.getElementById("login-form");
+const signupContainer = document.getElementById("signup-container");
+const loginContainer = document.getElementById("login-container");
 
-    // Inizializza visibilità
-    signupForm.style.display = "none"; // Nasconde il form di signup
+// Inizializza visibilità
+signupForm.style.display = "none"; // Nasconde il form di signup
 
-    // Aggiungi evento al radio button
-    signupRadio.addEventListener("change", function () {
-        if (signupRadio.checked) {
-            signupForm.style.display = "block";
-            signupContainer.style.backgroundColor = "white";
-            loginForm.style.display = "none";
-            loginContainer.style.backgroundColor = "#bdbdbd";
-        }
-    });
+// Aggiungi evento al radio button
+signupRadio.addEventListener("change", function () {
+    if (signupRadio.checked) {
+        signupForm.style.display = "block";
+        signupContainer.style.backgroundColor = "white";
+        loginForm.style.display = "none";
+        loginContainer.style.backgroundColor = "#bdbdbd";
+    }
+});
 
-    loginRadio.addEventListener("change", function () {
-        if (loginRadio.checked) {
-            loginForm.style.display = "block";
-            loginContainer.style.backgroundColor = "white";
-            signupForm.style.display = "none";
-            signupContainer.style.backgroundColor = "#bdbdbd";
-        }
-    });
+loginRadio.addEventListener("change", function () {
+    if (loginRadio.checked) {
+        loginForm.style.display = "block";
+        loginContainer.style.backgroundColor = "white";
+        signupForm.style.display = "none";
+        signupContainer.style.backgroundColor = "#bdbdbd";
+    }
 });
---
+++ venditore_script.js
@@ -0,0 +1,57 @@
+const editButton = document.querySelectorAll(
+    "main > div > div > section > div > div > div > div > a:first-of-type"
+);
+const deleteButton = document.querySelectorAll(
+    "main > div > div > section > div > div > div > div > a:last-of-type"
+);
+const addButton = document.querySelector("main > div > div > section > div > button");
+const articlesPane = document.querySelector("main > ul > li:first-of-type");
+const orderPane = document.querySelector("main > ul > li:last-of-type");
+const articleTab = document.querySelector("main > div > div:first-of-type");
+
+addButton.addEventListener("click", () => {
+    if (window.innerWidth > 768) {
+        document.querySelector("main > div > div").innerHTML += `
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="addModalLabel">Aggiungi prodotto:</h1>
+            </div>
+            <div class="modal-body">
+                <form action="">
+                    <div class="mb-3">
+                        <label for="product-name" class="form-label">Nome:</label>
+                        <input type="text" class="form-control" id="product-name">
+                    </div>
+                    <div class="mb-3">
+                        <label for="product-price" class="form-label">Prezzo:</label>
+                        <input type="text" class="form-control" id="product-price">
+                    </div>
+                    <div class="mb-3">
+                        <label for="product-quantity" class="form-label">Quantità:</label>
+                        <input type="text" class="form-control" id="product-quantity">
+                    </div>
+                    <div class="mb-3">
+                        <label for="product-quantity" class="form-label">Sconto:</label>
+                        <input type="text" class="form-control" id="product-quantity">
+                    </div>
+                    <div class="mb-3">
+                        <label for="product-quantity" class="form-label">Immagine:</label>
+                        <input type="file" class="form-control" id="product-quantity">
+                    </div>
+                    <div class="mb-3">
+                        <label for="product-description" class="form-label">Descrizione:</label>
+                        <textarea class="form-control" id="product-description" rows="3"></textarea>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn" data-bs-dismiss="modal">Chiudi</button>
+                <button type="button" class="btn">Salva</button>
+            </div>
+        `;
+    }
+    console.log("É stato cliccato il bottone modifica");
+});
+
+orderPane.addEventListener("shown.bs.tab", () => {
+   articleTab.style.setProperty("display", "none", "!important");
+});
