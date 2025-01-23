c25d52a6d8414d9fac1ad97c455bb48cfc103b6c
Marco Paggetti <marco.paggetti@studio.unibo.it>
2025-01-23 20:02:51 +0100

    Added products for a given category

---
+++ api/api-prodotti-categoria.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once('../php/bootstrap.php');
+
+if(isset($_GET["Categoria"])) {
+    $categoria = $_GET["Categoria"];
+}
+
+$articoliPerCategoria = $dbh->getProductsFromCategory($categoria);
+
+for($i = 0; $i < count($articoliPerCategoria); $i++) {
+    $articoliPerCategoria[$i]["Nome_immagine"] = UPLOAD_DIR.$articoliPerCategoria[$i]["Nome_immagine"];
+}
+
+header('Content-Type: application/json');
+echo json_encode($articoliPerCategoria);
+
+?>
