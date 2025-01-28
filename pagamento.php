6bfef19eab77534ec3ce0a6a3c1d7fc93992cda1
Marco Paggetti <marco.paggetti@studio.unibo.it>
2025-01-28 13:17:23 +0100

    Modified string pagamento title

--- pagamento.php
+++ pagamento.php
@@ -15,7 +15,7 @@
 <!DOCTYPE html>
 <html lang="it">
 <head>
-    <title>Scout Shop - Home</title>
+    <title>Scout Shop - Pagamento</title>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
